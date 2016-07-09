078e23e5b15865de558adcb93d19d944b7c19135
Julio César Echeverri Marulanda <julio.em7@gmail.com>
2016-07-09 09:14:01 -0500

    El programa valida la existencia de un archivo y descomprime, se han agregado estos nuevos tipos de dato para trabajar con tablas y columnas del archivo de de libreoffice

---
+++ headers/LatexTypes.h
@@ -0,0 +1,34 @@
+#ifndef _LATEX_TYPES_H_
+#define _LATEX_TYPES_H_
+
+namespace lt //Namespace for Latex Types
+{
+	struct CellStyle
+	{
+		bool italic;
+		bool strong;
+		bool center;	
+	};
+
+	struct Cell
+	{
+		std::string content;
+		bool hBackgroundColor; //do have background color?
+		bool hForegroundColor; //do have foreground color?
+	};
+
+	struct Table
+	{
+		std::string title;
+		std::string label;
+		std::string caption;
+		bool centerInText;	
+
+		int nCells;             //Number of cells in the table
+		int nCols;				//Number of nCols in the table
+		int nRows;				//Number of nRows in the table
+	};
+
+}
+
+#endif
